6af9af6ba72502030f718e5ff957315362274703
natalijanatt <168548423+natalijanatt@users.noreply.github.com>
2025-08-11 13:16:57 +0200

    logger

--- share-bills-back/src/main.ts
+++ share-bills-back/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import corsOptions from './config/corsOptions';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useLogger(app.get(Logger))
   await app.listen(process.env.PORT ?? 3000);
   app.enableCors({
     corsOptions
