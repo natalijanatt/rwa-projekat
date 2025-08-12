62ebe6b7a9aa64067f7b322a8bb2c18eee3f3d3a
natalijanatt <168548423+natalijanatt@users.noreply.github.com>
2025-08-13 00:28:33 +0200

    Generic button & loader

--- share-bills-front/src/app/app.component.ts
+++ share-bills-front/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from './shared/components/button/button.component';
+import { LoaderComponent } from './shared/components/loader/loader.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'share-bills-front';
+
+  onClick(){
+    console.log('Button clicked!');
+  }
 }
--- share-bills-front/src/app/shared/components/button/button.component.ts
+++ share-bills-front/src/app/shared/components/button/button.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgClass, NgIf } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [NgClass, NgIf],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.scss'
+  styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
+  @Input() label: string = '';
+  @Input() variant: 'primary' | 'secondary' | 'danger' = 'primary';
+  @Input() disabled: boolean = false;
+  @Input() size: 'small' | 'medium' | 'large' = 'medium';
+  @Input() type: 'button' | 'submit' = 'button';
+  @Input() icon: string | null = null;
+  @Input() loading: boolean = false;
+
+  @Output() clicked = new EventEmitter<MouseEvent>()
+
+  onClick(event: MouseEvent){
+    if (!this.disabled && !this.loading) {
+      this.clicked.emit(event);
+    }
+  }
 
 }
+
