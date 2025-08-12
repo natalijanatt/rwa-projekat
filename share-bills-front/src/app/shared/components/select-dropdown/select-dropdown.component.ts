fe55d9d0d06934220369fa4b20dd7b503188cec7
natalijanatt <168548423+natalijanatt@users.noreply.github.com>
2025-08-12 21:54:54 +0200

    switch to scss

---
+++ share-bills-front/src/app/shared/components/select-dropdown/select-dropdown.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select-dropdown',
+  imports: [],
+  templateUrl: './select-dropdown.component.html',
+  styleUrl: './select-dropdown.component.scss'
+})
+export class SelectDropdownComponent {
+
+}
